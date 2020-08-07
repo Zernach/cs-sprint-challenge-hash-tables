264596f2e8de8284a9ded957eb069b5a26097c0e
Zernach <ryan@zernach.com>
2020-08-07 12:10:30 -0400

    ex3 not yet passing all tests, coming soon

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,17 +1,29 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
 
-    return result
+    # Instantiate a list of dictionaries (hash table) to count views-per-digit in each array.
+    view_counts = [{}, {}, {}]
+
+    # For each of the arrays, check values, create each value, set equal to 1 (it exists)
+    for arr in range(0, len(arrays)):
+        for i in arrays[arr]:
+            view_counts[arr][i] = 1
+
+    # Not passing tests yet.
+    intersections = []
+    for dic in range(0, len(view_counts)):
+        for i in view_counts[dic]:
+            if i in view_counts[0:2]:
+                if i not in intersections:
+                    intersections.append(view_counts[dic][i][0])
+
+    return intersections
 
 
 if __name__ == "__main__":
     arrays = []
 
-    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
-    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
-    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+    arrays.append(list(range(1000000,2000000)) + [1, 2, 3])
+    arrays.append(list(range(2000000,3000000)) + [1, 2, 3])
+    arrays.append(list(range(3000000,4000000)) + [1, 2, 3])
 
     print(intersection(arrays))
